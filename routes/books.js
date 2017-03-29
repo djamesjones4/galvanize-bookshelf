49c3dd8d768c928e47a0f86420c703a41a416bea
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-03-29 15:55:02 -0600

    part 1 and 2 passed getting books

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex')
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
+const pg = require('pg')
+const humps = require('humps')
 // YOUR CODE HERE
+router.get('/books', function (req, res, next) {
+  knex('books')
+  .orderBy('title', 'asc')
+  .then((books)=>{
+// console.log(books);
+    res.send(humps.camelizeKeys(books))
+  })
+  .catch((err)=> {
+    next(err)
+  })
 
+})
 module.exports = router;
