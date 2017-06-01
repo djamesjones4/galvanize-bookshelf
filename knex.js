f9d466bf12f72ffd3c2786eae53eca9b959cfd26
Derek Jones <djamesjones4@gmail.com>
2017-06-01 11:18:55 -0600

    knex.js reverted previous change

--- knex.js
+++ knex.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const environment = process.env.NODE_ENV || 'development';
-const knexConfig = psqlConfig[environment];
+const knexConfig = require('./knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
 module.exports = knex;
